730a855027ecb910246c86475e10f427c3e25ca1
Iver <iver.ls@hotmail.no>
2024-10-07 22:30:50 +0000

    Added a for loop for fetching weather from multiple location, as well as displaying them

--- weather.js
+++ weather.js
@@ -1,12 +1,53 @@
-fetch(
-  `https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current_weather=true`,
-)
-  .then((response) => response.json())
-  .then((weather) => {
-    document.querySelector(".weather-container").innerHTML = `
-      <h3>Current Weather in Tokyo</h3>
-      <p>Temperature: ${weather.current_weather.temperature} °C</p>
-      <p>Wind Speed: ${weather.current_weather.windspeed} km/h</p>
-      <p>Wind Direction: ${weather.current_weather.winddirection}°</p>
-      <p>Observation Time: ${weather.current_weather.time}</p>`;
-  });
+const locations = [
+  {
+    name: "Tokyo",
+    latitude: 35.6895,
+    longitude: 139.6917,
+  },
+  {
+    name: "Gjøvik",
+    latitude: 60.7963,
+    longitude: 10.68452,
+  },
+  {
+    name: "London",
+    latitude: 51.5074,
+    longitude: -0.1278,
+  },
+  {
+    name: "Sydney",
+    latitude: -33.8688,
+    longitude: 151.2093,
+  },
+  {
+    name: "Cairo",
+    latitude: 30.0444,
+    longitude: 31.2357,
+  },
+];
+
+locations.forEach((location) => {
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current_weather=true`,
+  )
+    .then((response) => response.json())
+    .then((weatherData) => {
+      displayWeather(weatherData, location.name);
+    });
+});
+
+function displayWeather(weatherData, locationName) {
+  const container = document.querySelector(".weather-container");
+
+  const weatherDiv = document.createElement("div");
+  weatherDiv.classList.add("weather-location");
+
+  weatherDiv.innerHTML = `
+      <h3>Current Weather in ${locationName}</h3>
+      <p>Temperature: ${weatherData.current_weather.temperature} °C</p>
+      <p>Wind Speed: ${weatherData.current_weather.windspeed} km/h</p>
+      <p>Wind Direction: ${weatherData.current_weather.winddirection}°</p>
+      <p>Observation Time: ${weatherData.current_weather.time}</p>`;
+
+  container.appendChild(weatherDiv);
+}
