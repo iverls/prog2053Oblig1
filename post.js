727653a70fbf29e34e731ee5b1a551031fd08360
Iver <iver.ls@hotmail.no>
2024-10-07 20:51:33 +0000

    Added an eventlistener for when user scroll to bottom of page, which trigger fetching of more posts

--- post.js
+++ post.js
@@ -1,18 +1,38 @@
+let currentPage = 1;
 const postsPerPage = 12;
+let isLoading = false;
 
-fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsPerPage}`)
-  .then((response) => response.json())
-  .then((posts) => {
-    const postContainer = document.querySelector(".post-container");
+function fetchPosts() {
+  if (isLoading) return;
+  isLoading = true;
+  fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsPerPage}`)
+    .then((response) => response.json())
+    .then((posts) => {
+      const postContainer = document.querySelector(".post-container");
 
-    posts.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.classList.add("post");
+      posts.forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
 
-      postElement.innerHTML = `
+        postElement.innerHTML = `
 <h3>${post.title}</h3>
 <p>${post.body}</p>`;
 
-      postContainer.appendChild(postElement);
+        postContainer.appendChild(postElement);
+      });
+      currentPage++;
+      isLoading = false;
     });
-  });
+}
+
+function handleScroll() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    fetchPosts();
+  }
+}
+
+fetchPosts();
+
+globalThis.addEventListener("scroll", handleScroll);
