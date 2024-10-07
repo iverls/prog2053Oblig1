257cf2c048653c60ae15e83507b43bf9859c1c14
Iver <iver.ls@hotmail.no>
2024-10-07 19:06:56 +0000

    Modified js script to fetch 12 posts at once

--- post.js
+++ post.js
@@ -1,5 +1,18 @@
-fetch("https://jsonplaceholder.typicode.com/posts/1")
+const postsPerPage = 12;
+
+fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsPerPage}`)
   .then((response) => response.json())
-  .then((data) => {
-    document.querySelector(".post-container").innerText = data.title;
+  .then((posts) => {
+    const postContainer = document.querySelector(".post-container");
+
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.classList.add("post");
+
+      postElement.innerHTML = `
+<h3>${post.title}</h3>
+<p>${post.body}</p>`;
+
+      postContainer.appendChild(postElement);
+    });
   });
